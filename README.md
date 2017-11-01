b327cdde218f97cdf8de7a81d51d0ccc1c571d6e
Joshua Lawson <ordinarycryptid@gmail.com>
2017-11-01 05:19:27 -0400

    initial add

---
+++ README.md
@@ -0,0 +1,28 @@
+# coffee-shop-jquery
+A remake of the coffee shop from cms-esque using jQuery.
+
+### Modify your recipe app to add some advanced features with jQuery.
+
+Modify your menu app to include some new features enabled by jQuery. See the sample code at the bottom for all the scripts you'll need to include. I'd suggest starting with step 1, then doing step 3, then doing step 2.
+
+1. Add a new property to each food, which includes a set of strings describing categories for the food. Potential categories include vegetarian, low-cal, and gluten-free. You should be able to add one of these categories to a new food using jQuery autocomplete (documentation here). Show the properties in the DOM with the food.
+2. Instead of using class="hidden" to show/hide multiple views, use jQuery tabs instead. Here's the documentation. You should also add a third tab, which should display vegetarian-friendly food only ('vegetarian' being one of the categories).
+
+## Using jQuery
+
+We'll be using jQuery and jQuery UI (a popular plugin to jQuery) for tonight's assignment. You'll need to include these JS files to get both libraries:
+``` html
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+  crossorigin="anonymous"></script>
+  ```
+  Plugins just add additional features to jQuery. jQuery UI adds a a collection of user interface-related features listed on the [left-hand side of their page](jqueryui.com). In this case, the most important resource will be the documentation pages on autocomplete and tabs; in particular click the 'view source' link in each page to see examples.
+
+## Hard mode
+
+Make it possible to add any number of properties to each food.
---
+++ js/app.js
@@ -0,0 +1,61 @@
+let coffee = require('./coffee');
+
+window.addEventListener('load', function () {
+    console.log('tied together!');
+    
+    let menu = document.querySelector('.menu');
+    let addItem = document.querySelector('.addItem');
+    let menadd = document.querySelector('#menuTab');
+    let addTab = document.querySelector('#addTab');
+    let btn = document.querySelector('button');
+    let counter = 0;
+
+    function addItems() {
+        for (let i = counter; i < coffee.length; i++) {
+            let menuItem = document.createElement('ul');
+            let name = document.createElement('li');
+            let title = document.createElement('h2');
+            let desc = document.createElement('li');
+            let price = document.createElement('li');
+            desc.classList.add('desc');
+            title.textContent = coffee[i].name;
+            desc.textContent = coffee[i].description;
+            price.textContent = coffee[i].price;
+            menu.appendChild(menuItem);
+            name.appendChild(title);
+            menuItem.appendChild(name);
+            menuItem.appendChild(desc);
+            menuItem.appendChild(price);
+            counter++
+        };
+    };
+
+    addItems();
+
+    menuTab.addEventListener('click',function() {
+        if (menu.classList.contains('hidden')) {
+            menu.classList.remove('hidden');
+            addItem.classList.add('hidden');
+        }
+    });
+
+    addTab.addEventListener('click',function() {
+        if (addItem.classList.contains('hidden')) {
+            addItem.classList.remove('hidden');
+            menu.classList.add('hidden');
+        };
+    });
+
+    btn.addEventListener('click', function() {
+        let name = document.querySelector('#name');
+        let desc = document.querySelector('#desc');
+        let price = document.querySelector('#price');
+        let newCoffee = { name: name.value, description: desc.value, price: `$` + price.value };
+        coffee.push(newCoffee);
+        addItems();
+        name.value = "";
+        desc.value = "";
+        price.value = "";
+    });
+
+});
