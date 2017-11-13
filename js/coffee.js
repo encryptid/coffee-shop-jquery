d5f5df7273a53c528273f49f6b215572ea1a03d6
Joshua Lawson <ordinarycryptid@gmail.com>
2017-11-12 20:19:36 -0500

    updated coffee object

--- js/coffee.js
+++ js/coffee.js
@@ -1,10 +1,10 @@
 module.exports = [
-    { name: 'Pourover', description: 'Your favorite bean, fresh ground with hot water poured over it.', price: '$3' },
-    { name: 'Americano', description: "Espresso and water. Don't trust your barista? This is how you ensure your coffee is fresh.", price: '$3' },
-    { name: 'Aeropress', description: "Hot water, forced thru coffee grounds. Sort of like espresso, but not exactly.", price: '$3' },
-    { name: 'Vacuum Pot', description: "Remember in Breaking Bad where that Gale Boetticher guy made coffee in this crazy contraption? This is kinda like that. Come for the coffee, stay for the show.", price: '$4' },
-    { name: 'Fall Latte', description: 'Cinnamon, cardamom, star anise, baby powder, and vanilla bean all hung out in a jar to one day come together into this foamy concoction.', price: '$4.50' },
-    { name: 'Espresso Mule', description: 'You know, I thought it sounded gross at first, too, but ginger beer, lime and espresso go together better than you would think.', price: '$5' },
-    { name: 'Cold Brew', description: "I don't believe coffee should be served cold, but I made it for you, anyway because I'm nice.", price: '$2.50' },
-    { name: 'Nitro Cold Brew', description: "If coffee, science, and marketing had a baby, it would be this. And like babies, everybody pretends it's cuter than it actually is.", price: '$3.50' },
+    { name: 'Pourover', description: 'Your favorite bean, fresh ground with hot water poured over it.', price: '$3', tags: ['coffee', 'dairy-free'] },
+    { name: 'Americano', description: "Espresso and water. Don't trust your barista? This is how you ensure your coffee is fresh.", price: '$3', tags: ['espresso', 'dairy-free'] },
+    { name: 'Aeropress', description: "Hot water, forced thru coffee grounds. Sort of like espresso, but not exactly.", price: '$3', tags: ['coffee', 'espresso', 'dairy-free'] },
+    { name: 'Vacuum Pot', description: "Remember in Breaking Bad where that Gale Boetticher guy made coffee in this crazy contraption? This is kinda like that. Come for the coffee, stay for the show.", price: '$4', tags: ['coffee', 'dairy-free'] },
+    { name: 'Fall Latte', description: 'Cinnamon, cardamom, star anise, baby powder, and vanilla bean all hung out in a jar to one day come together into this foamy concoction.', price: '$4.50', tags: ['espresso', 'lightly sweet', 'vanilla'] },
+    { name: 'Espresso Mule', description: 'You know, I thought it sounded gross at first, too, but ginger beer, lime and espresso go together better than you would think.', price: '$5', tags: ['espresso', 'dairy-free', 'cold','lightly sweet', 'refreshing', 'ginger beer', 'lime'] },
+    { name: 'Cold Brew', description: "I don't believe coffee should be served cold, but I made it for you, anyway because I'm nice.", price: '$2.50', tags: ['cold', 'refreshing', 'dairy-free'] },
+    { name: 'Nitro Cold Brew', description: "If coffee, science, and marketing had a baby, it would be this. And like babies, everybody pretends it's cuter than it actually is.", price: '$3.50', tags: ['cold', 'refreshing', 'dairy-free'] },
 ];
